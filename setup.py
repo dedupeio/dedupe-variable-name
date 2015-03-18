30282dfe3137de00fae62a8f282509c6404769c5
Forest Gregg <fgregg@uchicago.edu>
2015-03-18 13:44:37 -0500

    start from usaddress

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ except ImportError :
 
 
 setup(
-    name='dedupe-variable-address',
-    url='https://github.com/datamade/dedupe-variables-address',
-    version='0.0.2',
-    description='Address variable type for dedupe',
+    name='dedupe-variable-name',
+    url='https://github.com/datamade/dedupe-variables-name',
+    version='0.0.1',
+    description='Name variable type for dedupe',
     packages=['dedupe.variables'],
-    install_requires=['usaddress', 'dedupe', 'future'],
+    install_requires=['probablepeople', 'dedupe', 'future'],
     license='The MIT License: http://www.opensource.org/licenses/mit-license.php'
     )
