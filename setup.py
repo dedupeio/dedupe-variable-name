e1e8420c40c9e47d8faed0f32b44066c62ac7108
Forest Gregg <fgregg@users.noreply.github.com>
2022-01-11 15:44:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError :
 setup(
     name='dedupe-variable-name',
     url='https://github.com/datamade/dedupe-variables-name',
-    version='0.0.13',
+    version='0.0.14',
     description='Name variable type for dedupe',
     packages=['dedupe.variables'],
     install_requires=['probablepeople >= 0.5', 
