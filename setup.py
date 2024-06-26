c6fea938391263ce6868ef328dae7dca3b0cdea9
Forest Gregg <fgregg@datamade.us>
2024-06-26 11:20:17 -0400

    droping namespace packaging pattern

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 try:
     from setuptools import setup
-except ImportError :
-    raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
+except ImportError:
+    raise ImportError(
+        "setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools"
+    )
 
 
 setup(
-    name='dedupe-variable-name',
-    url='https://github.com/datamade/dedupe-variables-name',
-    version='0.0.14',
-    description='Name variable type for dedupe',
-    packages=['dedupe.variables'],
-    install_requires=['probablepeople >= 0.5', 
-                      'parseratorvariable >= 0.0.18',
-                      'future'],
-    license='The MIT License: http://www.opensource.org/licenses/mit-license.php'
-    )
+    name="dedupe-variable-name",
+    url="https://github.com/datamade/dedupe-variables-name",
+    version="1.0.0",
+    description="Name variable type for dedupe",
+    packages=["namevariable"],
+    install_requires=["probablepeople >= 0.5", "parseratorvariable >= 1.0.0"],
+    license="The MIT License: http://www.opensource.org/licenses/mit-license.php",
+)
